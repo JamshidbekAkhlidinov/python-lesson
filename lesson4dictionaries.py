182c44705ecef27141992ef7e58691e68eed269f
JamshidbekAkhlidinov <jamshidbekakhlidinov@gmail.com>
2023-10-07 00:04:58 +0500

    thisdict

---
+++ lesson4dictionaries.py
@@ -0,0 +1,12 @@
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+print(thisdict)
+print(thisdict['brand'])
+print(thisdict.keys())
+print(thisdict.values())
+print(type(thisdict))
